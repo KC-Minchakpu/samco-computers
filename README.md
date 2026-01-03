2aef0870e3d7e245d4a851d8213f43f4a386a2c9
KC-Minchakpu <octatesystems@outlook.com>
2026-01-03 18:45:21 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# SAMCO COMPUTERS
+
+## Project
+
+1. Setup
+2. Install Tools
+3. Create Typescript React App Using Vite
+4. Create Git Repository
---
+++ frontend/src/App.tsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div>
+        <header>Samco Computers</header>
+        <main></main>
+        <footer>
+          &copy; 2026 Samco Computers. All Rights Reserved. Designed and
+          Developed by Octate Systems.
+        </footer>
+      </div>
+    </>
+  )
+}
+
+export default App
